cc59a9baad6f86bd1f5a085bb37c5a45a0bacce4
Luis Mata Salvatierra <luis.mata@coverwallet.com>
2018-10-18 10:57:51 +0200

    Add config files

---
+++ example.zsh
@@ -0,0 +1,13 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
+
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='fg=84,bold'
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='fg=9,bold'
